97d0f0aa03a4f49d94d5ec7d9ff8b1da93b7775c
Gelm20 <gilelopez@hotmail.com>
2021-02-23 20:33:17 -0600

    firstPush

---
+++ pregunta3/script.js
@@ -0,0 +1,59 @@
+
+function processForm() {
+    var name = document.getElementById("name").value;
+    var orientacion = document.getElementById("orientacion").value;
+    var cantidad = parseInt(document.getElementById("cantidad").value);
+    var estado = "";
+    
+    if (cantidad > 50) {
+        estado = "50+"
+    } else if (cantidad < 50) {
+        estado = "50-"
+    } else if (cantidad == 50) {
+        estado = "50="
+    }
+    tabla = document.getElementById("tabla").innerHTML
+    
+    document.getElementById("tabla").innerHTML = tabla + "<tr> <td>" + name + "</td> <td>" + orientacion + "</td> <td>" + cantidad + "</td> <td>" + estado + "</td> </tr>"
+
+    var partidoArray = [];
+
+    if (sessionStorage.getItem("wPartidoArray") !== null) {
+        partidoArray = JSON.parse(sessionStorage.getItem("wPartidoArray"));
+    }
+
+    var currentPartidos = {
+        partido: name,
+        votos: cantidad
+    };
+
+    partidoArray.push(currentPartidos);
+
+    sessionStorage.setItem("wPartidoArray", JSON.stringify(partidoArray));
+}
+
+function ganador() {
+
+    var partidoArray = JSON.parse(sessionStorage.getItem("wPartidoArray"));
+    
+    var votesArray = [];
+
+    var i;
+
+    for (i = 0; i < partidoArray.length; i++) {
+        votesArray.push(partidoArray[i].votos);
+      }
+
+    alert(votesArray)
+
+    var votesArray2 = votesArray;
+
+    votesArray2.sort(function(a, b){return a - b});
+
+    var min = votesArray.indexOf(votesArray2[0]);
+    var max = votesArray.indexOf(votesArray2[votesArray2.length -1]);
+
+    alert("El ganador es el partido " + partidoArray[min].partido + " con " + partidoArray[min].votos + " votos")
+    alert("El partido con menos votos fue " + partidoArray[max].partido + " con " + partidoArray[max].votos + " votos")
+}
+
